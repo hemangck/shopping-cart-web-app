09fa5d5d5f5a2d4af569ec050cf72510e4d706ad
Hemang <hemang.kulkarni@somaiya.edu>
2026-05-15 14:26:27 +0530

    Prepared backend for deployment

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // getting port 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // testing port
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
